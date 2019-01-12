54952363ae038271a7e36ca79eb10ab20c361dd4
BMarx981 <b.marx981@gmail.com>
2019-01-12 17:11:20 -0500

    Change for json messages

--- ws.go
+++ ws.go
@@ -8,18 +8,16 @@ import (
 	"golang.org/x/net/websocket"
 )
 
-func Echo(ws *websocket.Conn) {
+func Receive(ws *websocket.Conn) {
 	var err error
-	var reply string
-	var num int
-	num = 0
+	var json string
 
 	for {
-		if err = websocket.Message.Receive(ws, &reply); err != nil {
+		if err = websocket.Message.Receive(ws, &json); err != nil {
 			fmt.Println("Can't receive")
 			break
 		}
-		num += 1
+
 		fmt.Printf("%s \n", reply)
 
 		// if err = websocket.Message.Send(ws, reply); err != nil {
@@ -30,7 +28,7 @@ func Echo(ws *websocket.Conn) {
 }
 
 func main() {
-	http.Handle("/", websocket.Handler(Echo))
+	http.Handle("/", websocket.Handler(Receive))
 
 	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
