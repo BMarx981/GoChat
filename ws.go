448d1645ca720f596403f742e6c1c6f7fb64d847
BMarx981 <b.marx981@gmail.com>
2019-01-12 16:28:44 -0500

    Receive json text

--- ws.go
+++ ws.go
@@ -5,74 +5,32 @@ import (
 	"log"
 	"net/http"
 
-	// "github.com/gorilla/websocket"
 	"golang.org/x/net/websocket"
 )
-
-// var upgrader = websocket.Upgrader{
-// 	ReadBufferSize:  1024,
-// 	WriteBufferSize: 1024,
-// }
-//
-// func handler(w http.ResponseWriter, r *http.Request) {
-// 	conn, err := upgrader.Upgrade(w, r, nil)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		return
-// 	}
-//
-// 	for {
-// 		messageType, reader, err := conn.NextReader()
-// 		if err != nil {
-// 			fmt.Println(err)
-// 			return
-// 		}
-// 		fmt.Println()
-// 		writer, err := conn.NextWriter(messageType)
-// 		if err != nil {
-// 			fmt.Println(err)
-// 			return
-// 		}
-//
-// 		if _, err := io.Copy(writer, reader); err != nil {
-// 			return
-// 		}
-// 		fmt.Println(writer.Write(p))
-// 		if err := writer.Close(); err != nil {
-// 			return
-// 		}
-// 	}
-// }
 
 func Echo(ws *websocket.Conn) {
 	var err error
 	var reply string
+	var num int
+	num = 0
 
-	// for {
-	if err = websocket.Message.Receive(ws, &reply); err != nil {
-		fmt.Println("Can't receive")
-		// break
+	for {
+		if err = websocket.Message.Receive(ws, &reply); err != nil {
+			fmt.Println("Can't receive")
+			break
+		}
+		num += 1
+		fmt.Printf("%s \n", reply)
+
+		// if err = websocket.Message.Send(ws, reply); err != nil {
+		// 	fmt.Println("Can't send")
+		// 	break
+		// }
 	}
-	fmt.Println(reply + " is the reply")
-	// }
-
-}
-
-func Send(ws *websocket.Conn) {
-	var err error
-	var reply string
-	if err = websocket.Message.Send(ws, reply); err != nil {
-		fmt.Println("Can't send")
-		// break
-	}
-
-	fmt.Println("Send to client: " + reply)
 }
 
 func main() {
-	http.Handler("/", Echo)
-	fmt.Println("Between Funcs")
-	http.HandleFunc("/", Send)
+	http.Handle("/", websocket.Handler(Echo))
 
 	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
