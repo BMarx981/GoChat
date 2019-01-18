60f53db52ca1f52ee40af20055675c3325dd06a9
BMarx981 <b.marx981@gmail.com>
2019-01-17 20:16:37 -0500

    "Json read write"

--- ws.go
+++ ws.go
@@ -1,70 +1,74 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
-	//"golang.org/x/net/websocket"
 )
 
-var broadcast = make(chan string)
+var broadcast = make(chan Message)
 var clients = make(map[*websocket.Conn]bool) //connected clients
 var upgrader = websocket.Upgrader{}
 
 type Message struct {
-	text      string   `json:"message"`
-	email     string   `json:"email"`
-	receivers []string `json:"receivers"`
-	userName  string   `json:"username"`
+	text string `json:"message"`
+	// email string `json:"email"`
+	// receivers []string `json:"receivers"`
+	userName string `json:"sender"`
 }
 
-func receiveMessages(write http.ResponseWriter, req *http.Request) {
+func receiveMessages(w http.ResponseWriter, r *http.Request) {
 	var err error
-	// var msg string
-	// var dat map[string]interface{}
-
-	ws, err := upgrader.Upgrade(write, req, nil)
+	// header := w.Header()
+	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	defer ws.Close()
+	clients[ws] = true
+
 	for {
-		text := getJsonMessage(ws)
-		fmt.Println(text)
-		broadcast <- text
-	}
-}
+		var msg Message
+		i := 1
 
-func getJsonMessage(sock *websocket.Conn) string {
-	_, byt, err := sock.ReadMessage()
-	if err != nil {
-		fmt.Println(err)
-	}
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			fmt.Println("Json read error ")
+			fmt.Println(err)
+		}
+		if i == 1 {
+			fmt.Println(msg)
+			i += 1
+		}
+		if err != nil {
+			log.Printf("error: %v", err)
+			delete(clients, ws)
+			break
+		}
 
-	var dat map[string]interface{}
-
-	if err := json.Unmarshal(byt, &dat); err != nil {
-		panic(err)
+		broadcast <- msg
 	}
-	text := dat["message"].(string)
-	return text
 }
 
 func handleMessages() {
+	i := 0
 	for {
 		//Grab the next message from the broadcast channel
-		text := <-broadcast
-		fmt.Println(text)
-		fmt.Println("Reached here")
+		byt := <-broadcast
+		if i == 1 {
+			fmt.Println("in handleMessages")
+			fmt.Println(byt)
+			i += 1
+		}
+
 		//Send it out to every client that is currently connected
 		for client := range clients {
-			err := client.WriteJSON(text)
+			err := client.WriteJSON(byt)
 			if err != nil {
-				log.Printf("error: %v", err)
+				fmt.Printf("error: %v", err)
 				client.Close()
 				delete(clients, client)
 			}
@@ -74,7 +78,6 @@ func handleMessages() {
 
 func main() {
 	http.HandleFunc("/", receiveMessages)
-
 	go handleMessages()
 
 	if err := http.ListenAndServe(":8081", nil); err != nil {
